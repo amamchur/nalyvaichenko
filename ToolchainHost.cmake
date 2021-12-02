9fa7d92075842a7e37725b69a80e779ccec4fc62
Andrii Mamchur <amamchur@users.noreply.github.com>
2021-12-02 18:05:22 +0200

    Implemented image to flash generator

--- ToolchainHost.cmake
+++ ToolchainHost.cmake
@@ -12,7 +12,7 @@ endfunction(add_mcu_executable)
 
 function(add_host_executable NAME)
     add_executable(${NAME} ${ARGN})
-    target_link_libraries(${NAME} PRIVATE Qt5::Widgets)
+    target_link_libraries(${NAME} PRIVATE Qt5::Widgets ${PNG_LIBRARY})
 endfunction(add_host_executable)
 
 function(add_cubemx_project NAME MCU)
--- src/emu/test.cpp
+++ src/emu/test.cpp
@@ -7,7 +7,10 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <png.h>
 #include <sstream>
+#include <zoal/gfx/renderer.hpp>
+#include <zoal/ic/sh1106.hpp>
 
 zoal::misc::flash_machine fm;
 
@@ -33,15 +36,72 @@ struct image_data {
     const size_t size;
 };
 
+using adapter = zoal::ic::sh1106_adapter_0<128, 64>;
+using graphics = zoal::gfx::renderer<uint8_t, adapter>;
+uint8_t canvas[1024];
+
+void read_png() {
+    FILE *fp = fopen("D:\\aaa\\a.png", "rb");
+    png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    if (png == nullptr) {
+        return;
+    }
+
+    png_infop info = png_create_info_struct(png);
+    if (info == nullptr) {
+        png_destroy_read_struct(&png, nullptr, nullptr);
+        return;
+    }
+
+    png_init_io(png, fp);
+    png_read_info(png, info);
+    auto width = png_get_image_width(png, info);
+    auto height = png_get_image_height(png, info);
+    auto color_type = png_get_color_type(png, info);
+    auto bit_depth = png_get_bit_depth(png, info);
+
+    std::cout << "width\t: " << width << std::endl;
+    std::cout << "height\t: " << height << std::endl;
+    std::cout << "color_type\t: " << (int)color_type << std::endl;
+    std::cout << "bit_depth\t: " << (int)bit_depth << std::endl;
+
+    auto row_pointers = (png_bytep *)malloc(sizeof(png_bytep) * height);
+    auto row_bytes = png_get_rowbytes(png, info);
+    for (size_t y = 0; y < height; y++) {
+        row_pointers[y] = (png_byte *)malloc(row_bytes);
+    }
+    png_read_image(png, row_pointers);
+
+    auto gfx = graphics::from_memory(canvas);
+    gfx->clear(0);
+    for (size_t y = 0; y < height; y++) {
+        png_bytep row = row_pointers[y];
+        for (size_t x = 0; x < width; x++) {
+            png_bytep px = &(row[x * 4]);
+            if (px[0] == 0) {
+                gfx->pixel(x, y, 1);
+            }
+        }
+    }
+
+    for (int y = 0; y < height; y++) {
+        free(row_pointers[y]);
+    }
+    free(row_pointers);
+    png_destroy_read_struct(&png, &info, nullptr);
+}
+
 int main(int argc, char *argv[]) {
+    read_png();
+
     fm.callback(fm_callback);
 
-    image_data images[] = {{1 * 4096, ecafe_logo, ecafe_logo_size}, {2 * 4096, epd_bitmap_ecafe, epd_bitmap_ecafe_size}};
+    image_data images[] = {{1 * 4096, canvas, ecafe_logo_size}, {2 * 4096, epd_bitmap_ecafe, epd_bitmap_ecafe_size}};
     std::stringstream ss;
-    auto image = images[1];
+    auto image = images[0];
     auto address = image.address;
 
-    ss << "erase_sector 2" << std::endl;
+    ss << "erase_sector 1" << std::endl;
 
     for (size_t i = 0; i < image.size; i++) {
         if ((i & 0xFF) == 0) {
